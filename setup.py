888b5eb1117007a2a4d4f7a6bae779dd83899821
Ian Dela Cruz <iandc76@gmail.com>
2023-12-16 10:34:17 +0800

    Auto version

--- setup.py
+++ setup.py
@@ -8,17 +8,37 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="cflare-ddns",
-    version="0.2.0",
+    version="{{VERSION_PLACEHOLDER}}",
     author="Ian Dela Cruz",
     author_email="iandc76@gmail.com",
     url="https://github.com/ianpogi5/cflare-ddns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="Yet another Cloudflare DDNS",
     packages=find_packages(),
     install_requires=["requests>=2.31.0"],
+    keywords=[
+        "cloudflare",
+        "ddns",
+        "dynamic",
+        "dns",
+    ],
     entry_points={
         "console_scripts": [
             "cflare-ddns=cflare_ddns.main:main",
         ],
     },
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Internet :: Name Service (DNS)",
+        "Topic :: System :: Networking",
+    ],
 )
